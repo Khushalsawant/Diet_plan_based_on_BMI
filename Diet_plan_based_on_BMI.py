df1cedc3c04d47d98e45a49768544b69cb6e9e58
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-09-16 08:51:15 +0530

    Add files via upload

---
+++ Diet_plan_based_on_BMI.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  9 07:07:09 2019
+
+@author: khushal
+"""
+
+# https://spoonacular.com/food-api/docs
+
+## https://www.dataquest.io/blog/web-scraping-tutorial-python/
+user_name ="khushal" #str(input('Enter the User name := '))    
+user_gender = "male" #str(input('Enter Your gender details (MALE/FEMALE) := '))    
+user_age = 27 # int(input('Please enter your Age in number := '))
+height = 1.94 #float(input('Please enter your height input meters(decimals) := '))
+weight = 109 #int(input('Please enter your weight input kg := '))
+meal_preference = "non-vegetarian" #str(input('Enter your meal preference (vegetarian/non-vegetarian/eggetarian) := '))    
+diabetic_flag = "no" #str(input("Are you diabetic (YES/NO) := "))
+
+def bmi_calculator(height,weight):
+    bmi = round((weight/(height**2)),2)
+    
+    if bmi <= 18.5:
+        bmi_flag = "underweight"
+        print('Your BMI is', bmi,',which means you are underweight.')
+    elif bmi > 18.5 and bmi < 25:
+        bmi_flag = "healthy"
+        print('Your BMI is', bmi,',which means you are normal.')
+    elif bmi > 25 and bmi < 30:
+        bmi_flag = "overweight"
+        print('your BMI is', bmi,',which means you are overweight.')
+    elif bmi > 30:
+        bmi_flag = "obese"
+        print('Your BMI is', bmi,',which means you are obese.')
+    else:
+        print('There is an error with your input')
+        print('Please check you have entered whole numbers\n'
+              'and decimals were asked.')
+    return bmi,bmi_flag
+
+def calculate_body_fat_percentage (bmi,age,gender):
+    if gender.lower() == 'female':
+        Body_Fat_Percentage = round(((1.20 * bmi) + (0.23 * age) - 5.4),2)
+        body_fat_flag = set_body_fat_flag(Body_Fat_Percentage,age,gender)
+    elif gender.lower() == 'male':
+        Body_Fat_Percentage = round(((1.20 * bmi) + (0.23 * age) - 16.2),2)
+        body_fat_flag = set_body_fat_flag(Body_Fat_Percentage,age,gender)
+    else: 
+        print('There is an error with your input')
+        print('Please check you have entered whole numbers\n'
+              'and decimals were asked.')    
+    return Body_Fat_Percentage,body_fat_flag
+
+def set_body_fat_flag(Body_Fat_Percentage,age,gender):
+    if gender.lower() == 'female':
+        if age >= 20 and age < 40:
+            
+            if Body_Fat_Percentage < 21 :
+                body_fat_flag = 'underfat'
+            elif Body_Fat_Percentage >= 21 and Body_Fat_Percentage < 33 :
+                body_fat_flag = 'healthy'
+            elif Body_Fat_Percentage >= 33 and Body_Fat_Percentage < 39 :
+                body_fat_flag = 'overweight'
+            else:
+                body_fat_flag = 'obese'
+                
+        elif age >= 41 and age < 60:
+            
+            if Body_Fat_Percentage < 23 :
+                body_fat_flag = 'underfat'
+            elif Body_Fat_Percentage >= 23 and Body_Fat_Percentage < 35 :
+                body_fat_flag = 'healthy'
+            elif Body_Fat_Percentage >= 35 and Body_Fat_Percentage < 40 :
+                body_fat_flag = 'overweight'
+            else:
+                body_fat_flag = 'obese'
+                
+        elif age >= 61 and age < 79:
+
+            if Body_Fat_Percentage < 24 :
+                body_fat_flag = 'underfat'
+            elif Body_Fat_Percentage >= 24 and Body_Fat_Percentage < 36 :
+                body_fat_flag = 'healthy'
+            elif Body_Fat_Percentage >= 36 and Body_Fat_Percentage < 42 :
+                body_fat_flag = 'overweight'
+            else:
+                body_fat_flag = 'obese'            
+            
+        else:
+            print("Kindly check the entered input")
+            
+    elif gender.lower() == 'male':
+        if age >= 20 and age < 40:
+            
+            if Body_Fat_Percentage < 8 :
+                body_fat_flag = 'underfat'
+            elif Body_Fat_Percentage >= 8 and Body_Fat_Percentage < 19 :
+                body_fat_flag = 'healthy'
+            elif Body_Fat_Percentage >= 19 and Body_Fat_Percentage < 25 :
+                body_fat_flag = 'overweight'
+            else:
+                body_fat_flag = 'obese'
+                
+        elif age >= 41 and age < 60:
+            
+            if Body_Fat_Percentage < 11 :
+                body_fat_flag = 'underfat'
+            elif Body_Fat_Percentage >= 11 and Body_Fat_Percentage < 22 :
+                body_fat_flag = 'healthy'
+            elif Body_Fat_Percentage >= 22 and Body_Fat_Percentage < 27 :
+                body_fat_flag = 'overweight'
+            else:
+                body_fat_flag = 'obese'
+                
+        elif age >= 61 and age < 79:
+
+            if Body_Fat_Percentage < 13 :
+                body_fat_flag = 'underfat'
+            elif Body_Fat_Percentage >= 13 and Body_Fat_Percentage < 25 :
+                body_fat_flag = 'healthy'
+            elif Body_Fat_Percentage >= 25 and Body_Fat_Percentage < 30 :
+                body_fat_flag = 'overweight'
+            else:
+                body_fat_flag = 'obese'            
+    
+        else:
+            print("Kindly check the entered input")
+    return body_fat_flag
+
+def create_dict_from_variables(user_name, user_gender,height,weight,bmi,bmi_flag,Body_Fat_Percentage,body_fat_flag,meal_preference,diabetic_flag):
+    return {'user_name':user_name,'user_gender':user_gender, 'height':height,'weight':weight,
+            'bmi':bmi,'bmi_flag':bmi_flag,'Body_Fat_Percentage':Body_Fat_Percentage,'body_fat_flag':body_fat_flag,
+            'meal_preference':meal_preference,
+            'diabetic_flag':diabetic_flag}
+
+
+bmi,bmi_flag = bmi_calculator(height,weight)
+Body_Fat_Percentage,body_fat_flag = calculate_body_fat_percentage (bmi,user_age,user_gender)
+user_info_dict = create_dict_from_variables(user_name.lower(),user_gender.lower(),height,weight,
+                                            bmi,bmi_flag.lower(),Body_Fat_Percentage,body_fat_flag.lower(),
+                                            meal_preference.lower(),diabetic_flag.lower())
+print("user_info_dict = ",user_info_dict)
