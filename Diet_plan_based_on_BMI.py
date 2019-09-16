0d58265b61f03f9b9fe6b599e209b876b5c466ed
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-09-16 09:25:11 +0530

    Add files via upload

--- Diet_plan_based_on_BMI.py
+++ Diet_plan_based_on_BMI.py
@@ -15,6 +15,12 @@ height = 1.94 #float(input('Please enter your height input meters(decimals) := '
 weight = 109 #int(input('Please enter your weight input kg := '))
 meal_preference = "non-vegetarian" #str(input('Enter your meal preference (vegetarian/non-vegetarian/eggetarian) := '))    
 diabetic_flag = "no" #str(input("Are you diabetic (YES/NO) := "))
+print(                           '1. Very Light = Typical Office job (sitting,studying,little walking throughout the day) \n',
+                                 '2. Light = Any job where you mostly walk or stand(teaching,shop/lab work,some walking throughout the day) \n',
+                                 '3. Moderate = Jobs requiring physical activity (landscaping,cleaning,maintaince,jogging,biking/working out 2hrs/day) \n',
+                                 '4. Heavy = Heavy manual labor (construction,dancer,athlete,hard physical activity min 4 hrs/day) \n',
+                                 '5. Very Heavy = Moderate to hard physical activity min 8 hrs/day \n')
+daily_activity_level = 'moderate' # str(input('Enter your Daily activity := \n'))
 
 def bmi_calculator(height,weight):
     bmi = round((weight/(height**2)),2)
@@ -132,9 +138,57 @@ def create_dict_from_variables(user_name, user_gender,height,weight,bmi,bmi_flag
             'meal_preference':meal_preference,
             'diabetic_flag':diabetic_flag}
 
+def calculate_your_daily_calorie_needs(weight,gender,Body_Fat_Percentage,daily_activity_level):
+    if daily_activity_level.lower() == 'very light':
+        activity_level = 1.3
+    elif daily_activity_level.lower() == 'light':
+        activity_level = 1.55
+    elif daily_activity_level.lower() == 'moderate':
+        activity_level = 1.65
+    elif daily_activity_level.lower() == 'heavy':
+        activity_level = 1.80
+    elif daily_activity_level.lower() == 'very heavy':
+        activity_level = 2.00
+    else:
+        print("Kindly check the entered input")
+        
+    if gender.lower() == 'female':
+        if Body_Fat_Percentage >=14  and  Body_Fat_Percentage < 19:
+            lean_factor = 1.0
+        elif Body_Fat_Percentage >=19  and  Body_Fat_Percentage < 29:
+            lean_factor = 0.95
+        elif Body_Fat_Percentage >=29  and  Body_Fat_Percentage <= 38:
+            lean_factor = 0.90
+        else:
+            lean_factor = 0.85
+            
+        Basal_Metabolic_Rate = (weight *0.9) * 24 * lean_factor
+        
+    elif gender.lower() == 'male':
+        
+        if Body_Fat_Percentage >=10  and  Body_Fat_Percentage < 15:
+            lean_factor = 1.0
+        elif Body_Fat_Percentage >=15  and  Body_Fat_Percentage < 21:
+            lean_factor = 0.95
+        elif Body_Fat_Percentage >=21  and  Body_Fat_Percentage <= 28:
+            lean_factor = 0.90
+        else:
+            lean_factor = 0.85        
+        Basal_Metabolic_Rate = (weight* 1.0) * 24 * lean_factor
+
+    else: 
+        print('There is an error with your input')
+        print('Please check you have entered whole numbers\n'
+              'and decimals were asked.')   
+    
+    daily_calorie_need = round((Basal_Metabolic_Rate * activity_level),2)
+    print("daily_calorie_need = ",daily_calorie_need)
+    return daily_calorie_need
 
 bmi,bmi_flag = bmi_calculator(height,weight)
 Body_Fat_Percentage,body_fat_flag = calculate_body_fat_percentage (bmi,user_age,user_gender)
+daily_calorie_need = calculate_your_daily_calorie_needs(weight,user_gender,Body_Fat_Percentage,daily_activity_level)
+
 user_info_dict = create_dict_from_variables(user_name.lower(),user_gender.lower(),height,weight,
                                             bmi,bmi_flag.lower(),Body_Fat_Percentage,body_fat_flag.lower(),
                                             meal_preference.lower(),diabetic_flag.lower())
